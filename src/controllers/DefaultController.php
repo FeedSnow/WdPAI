f3cdd87de5579015354b986b7b06248ad83e573a
FeedSnow <mateovip22262@gmail.com>
2023-05-31 15:30:27 +0200

    podstawowy routing

---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'AppController.php';
+
+class DefaultController extends AppController {
+    public function index()
+    {
+        // TODO display login.html
+        $this->render('login');
+    }
+
+    public function offers()
+    {
+        // TODO display offers.html
+        $this->render('offers');
+    }
+}
