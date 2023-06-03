9429984cf160f22f167af214af1654113104b46d
FeedSnow <mateovip22262@gmail.com>
2023-06-03 18:39:59 +0200

    routing

--- index.php
+++ index.php
@@ -7,4 +7,5 @@ $path = parse_url($path, PHP_URL_PATH);
 
 Routing::get('index', 'DefaultController');
 Routing::get('offers', 'DefaultController');
+Routing::get('register', 'DefaultController');
 Routing::run($path);
--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -3,15 +3,17 @@
 require_once 'AppController.php';
 
 class DefaultController extends AppController {
-    public function index()
-    {
+    public function index() {
         // TODO display login.html
         $this->render('login');
     }
 
-    public function offers()
-    {
+    public function offers() {
         // TODO display offers.html
         $this->render('offers');
+    }
+
+    public function register() {
+        $this->render('register');
     }
 }
